50fe6c057634478e4848c47545d0d35a491cc6e0
Kevin <691208yz@gmail.com>
2020-08-15 11:21:44 -0500

    init

---
+++ src/components/CapitalGainForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react";
+import Paper from "@material-ui/core/Paper";
+
+class CapitalGainForm extends Component {
+  render() {
+    return (
+      <Fragment>
+        <form>
+          <Paper>
+              <p>hi</p>
+          </Paper>
+        </form>
+      </Fragment>
+    );
+  }
+}
+
+export default CapitalGainForm;
