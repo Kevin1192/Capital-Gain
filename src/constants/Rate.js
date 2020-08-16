6b4a2b31c81c4658be506088a5c0bf0420e8c862
Kevin <691208yz@gmail.com>
2020-08-16 16:48:54 -0500

    capital gain rates long/short term four filing status

---
+++ src/constants/Rate.js
@@ -0,0 +1,82 @@
+const longTermRatesSingle = {
+    zeroPercent: 40000,
+    fifteenPercent: 441450,
+
+}
+
+const longTermRatesHead = {
+  zeroPercent: 53600,
+  fifteenPercent: 469050,
+};
+
+const longTermRatesMarriedJointly = {
+  zeroPercent: 80000,
+  fifteenPercent: 496600,
+};
+
+const longTermRatesMarriedSeparately = {
+  zeroPercent: 40000,
+  fifteenPercent: 248300,
+};
+
+const shortTermRatesSingle = {
+  tenPercent: 9875,
+  twelvePercent: 40125,
+  twentyTwoPercent: 85525,
+  twentyFourPercent: 163300,
+  thirtyTwoPercent: 207350,
+  thirtyFivePercent: 518400,
+};
+
+const shortTermRatesHead = {
+  tenPercent: 14100,
+  twelvePercent: 53700,
+  twentyTwoPercent: 85500,
+  twentyFourPercent: 163300,
+  thirtyTwoPercent: 207350,
+  thirtyFivePercent: 518400,
+};
+
+
+const shortTermRatesMarriedJointly = {
+  tenPercent: 19750,
+  twelvePercent: 80250,
+  twentyTwoPercent: 171050,
+  twentyFourPercent: 326600,
+  thirtyTwoPercent: 414700,
+  thirtyFivePercent: 622050,
+};
+
+const shortTermRatesMarriedSeparately = {
+  tenPercent: 9875,
+  twelvePercent: 40125,
+  twentyTwoPercent: 85525,
+  twentyFourPercent: 163300,
+  thirtyTwoPercent: 207350,
+  thirtyFivePercent: 311025,
+};
+
+
+
+const rates = {
+    single: {
+        longTerm: longTermRatesSingle,
+        shortTerm: shortTermRatesSingle,
+    },
+    headOfHousehold: {
+        longTerm: longTermRatesHead,
+        shortTerm: shortTermRatesHead,
+    },
+    marriedJointly: {
+        longTerm: longTermRatesMarriedJointly,
+        shortTerm: shortTermRatesMarriedJointly,
+    },
+    marriedSeparately: {
+        longTerm: longTermRatesMarriedSeparately,
+        shortTerm: shortTermRatesMarriedSeparately,
+    }
+
+}
+
+
+export default rates;
