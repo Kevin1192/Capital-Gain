ee5815aa668deaa29a11b99a23590d38a319e25e
Kevin <691208yz@gmail.com>
2020-08-23 21:01:43 -0500

    change rate to 2D arrays

--- src/constants/rates.js
+++ src/constants/rates.js
@@ -1,60 +1,69 @@
-const longTermRatesSingle = {
-    0: 40000,
-    0.15: 441450,
-
-}
-
-const longTermRatesHead = {
-  0: 53600,
-  0.15: 469050,
-};
-
-const longTermRatesMarriedJointly = {
-  0: 80000,
-  0.15: 496600,
-};
-
-const longTermRatesMarriedSeparately = {
-  0: 40000,
-  0.15: 248300,
-};
-
-const shortTermRatesSingle = {
-  0.10: 9875,
-  0.12: 40125,
-  0.22: 85525,
-  0.24: 163300,
-  0.32: 207350,
-  0.35: 518400,
-};
-
-const shortTermRatesHead = {
-  0.10: 14100,
-  0.12: 53700,
-  0.22: 85500,
-  0.24: 163300,
-  0.32: 207350,
-  0.35: 518400,
-};
+const longTermRatesSingle = [
+  [0, 40000],
+  [0.15, 441450],
+  [0.2, Infinity],
+];
 
 
-const shortTermRatesMarriedJointly = {
-  0.10: 19750,
-  0.12: 80250,
-  0.22: 171050,
-  0.24: 326600,
-  0.32: 414700,
-  0.35: 622050,
-};
+const longTermRatesHead = [
+  [0, 53600],
+  [0.15, 469050],
+  [0.2, Infinity],
+];
 
-const shortTermRatesMarriedSeparately = {
-  0.10: 9875,
-  0.12: 40125,
-  0.22: 85525,
-  0.24: 163300,
-  0.32: 207350,
-  0.35: 311025,
-};
+
+const longTermRatesMarriedJointly = [
+  [0, 80000],
+  [0.15, 496600],
+  [0.2, Infinity],
+];
+
+const longTermRatesMarriedSeparately = [
+  [0, 40000],
+  [0.15, 248300],
+  [0.2, Infinity],
+];
+
+
+const shortTermRatesSingle = [
+  [0.1, 9875],
+  [0.12, 40125],
+  [0.22, 85525],
+  [0.24, 163300],
+  [0.32, 207350],
+  [0.35, 518400],
+  [0.37, Infinity]
+];
+
+const shortTermRatesHead = [
+  [0.1, 14100],
+  [0.12, 53700],
+  [0.22, 85500],
+  [0.24, 163300],
+  [0.32, 207350],
+  [0.35, 518400],
+  [0.37, Infinity]
+];
+
+const shortTermRatesMarriedJointly = [
+  [0.1, 19750],
+  [0.12, 80250],
+  [0.22, 171050],
+  [0.24, 326600],
+  [0.32, 414700],
+  [0.35, 622050],
+  [0.37, Infinity]
+];
+
+const shortTermRatesMarriedSeparately = [
+  [0.1, 9875],
+  [0.12, 40125],
+  [0.22, 85525],
+  [0.24, 163300],
+  [0.32, 207350],
+  [0.35, 311025],
+  [0.37, Infinity]
+];
 
 const rates = {
   longTerm: {
