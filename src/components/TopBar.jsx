a56c0f14eba8a0cabb231acc0c07286e2948783a
Kevin <691208yz@gmail.com>
2020-09-02 19:07:52 -0500

    add navbar

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Topbar extends Component {
+    constructor(props);
+    super(props);
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <div id='topnav' >
+
+                </div>
+            </React.Fragment>
+        )
+    }
+}
