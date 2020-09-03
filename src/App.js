335dbf62a4fb5dbbf5901dfa2bbe6cee0e9ccffd
Kevin <691208yz@gmail.com>
2020-09-02 19:22:04 -0500

    add topbar scss files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import CapitalGainForm from './components/CapitalGainForm';
+import TopBar from './components/TopBar';
+
+
+
+
 function App() {
   return (
-    <CapitalGainForm />
-    );
+    <React.Fragment>
+      <TopBar />
+      <CapitalGainForm />
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 
 
 class Topbar extends Component {
-    constructor(props);
+    constructor(props) {
     super(props);
+    };
 
 
     render() {
         return (
             <React.Fragment>
                 <div id='topnav' >
-
+                    hi
                 </div>
             </React.Fragment>
         )
     }
 }
+
+export default Topbar;
