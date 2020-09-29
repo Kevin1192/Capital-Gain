d7ce4d19f5b206605953df339f347bf1429d2bb5
Kevin Zhou <zhouk@DataCenter.hambeach.com>
2020-09-29 15:30:31 -0500

    basic get and post working

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const Sequelize = require('sequelize');
 
+const sequelize = new Sequelize('postgres://zhouk:691208@localhost:5432/mydb');
 
 const app = express();
 
@@ -14,3 +15,54 @@ const db_link = process.env.DB_LINK;
 app.use(bodyParser.json());
 
 app.use(cors());
+
+
+sequelize
+.authenticate()
+.then(() => {
+    console.log('postgres sql successful')
+})
+.catch(err => {
+    console.log('Unable to connect');
+})
+
+const User = sequelize.define('user', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: Sequelize.STRING,
+    }
+});
+
+User.sync({ force: true });
+
+
+app.post('/user', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+
+        await newUser.save();
+
+        res.json({ user: newUser })
+    } catch(err) {
+        console.log(err);
+    }
+})
+
+app.get('/user/:userId', async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        const user = await User.findAll({
+            where: {
+                id: userId
+            }
+        });
+        res.json({ user });
+    } catch(err) {
+        console.error(err);
+    }
+})
+app.listen(3000,() => console.log("listening on") )
