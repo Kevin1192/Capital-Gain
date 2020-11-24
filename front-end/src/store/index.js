f63d4088508fda2f0a2035e82a59c0271cb5b4a5
Kevin Zhou <691208yz@gmail.com>
2020-11-24 11:41:23 -0600

    add redux devtool extension

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,11 +1,14 @@
 import rootReducer from './reducers';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk';
 
+
+const composedWithDev = composeWithDevTools(applyMiddleware(thunk))
 export function configureStore() {
     const store = createStore(
         rootReducer,
-        compose(applyMiddleware(thunk))
+        composedWithDev
     )
     return store;
 }
